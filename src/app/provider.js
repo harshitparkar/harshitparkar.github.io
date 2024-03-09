de6a72d2a7c009d43009d82b0172e4c345627709
harshitparkar <56834158+harshitparkar@users.noreply.github.com>
2024-03-09 10:45:22 +0530

    Revert "Merge pull request #1 from harshitparkar/Initial_Personalization"
    This reverts commit 9240228a32a7c6feedf4cc01d93d8f72da389869, reversing
    changes made to 3164c50853a489b39ad09784c37078344f46e90a.

--- src/app/provider.js
+++
@@ -1,6 +0,0 @@
-"use client";
-import { ThemeProvider } from "next-themes";
-
-export default function Provider({ children }) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
-}
--- src/app/utils/AppConstant.js
+++
@@ -1,4 +0,0 @@
-export const AppConstants = {
-  DARK_THEME: "dark",
-  LIGHT_THEME: "light",
-};
