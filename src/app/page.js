eef7ab402b8c6e5f32ce0a49de2c4d92259aab17
harshitparkar <56834158+harshitparkar@users.noreply.github.com>
2024-03-17 19:59:36 +0530

    Add Content & Footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
-import styles from "./page.module.css";
+import ContentMainPage from "./pageSections/content/ContentMainPage";
+import Footer from "./pageSections/footer/Footer";
 import Header from "./pageSections/header/Header";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <main>
+      <Header />
+      <ContentMainPage />
+      <Footer />
+    </main>
+  );
 }
