beea07ff9b7dda5533755e479aa6c193f625c759
harshitparkar <56834158+harshitparkar@users.noreply.github.com>
2024-03-17 19:55:10 +0530

    Add header

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,6 @@
 import styles from "./page.module.css";
+import Header from "./pageSections/header/Header";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.center}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <h2 className={styles.card}>Performing Live Changes</h2>
-          <p className={styles.description}>
-            This is my portfolio website. Currently, it is under development.
-            Stay tuned for updates.
-          </p>
-        </div>
-      </div>
-    </main>
-  );
+  return <Header />;
 }
