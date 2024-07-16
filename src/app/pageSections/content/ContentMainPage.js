6e2626990656847f294c70c3b7f42a44ea91c59b
harshitparkar <56834158+harshitparkar@users.noreply.github.com>
2024-07-16 15:04:09 +0530

    Develop - custom cursor

--- src/app/pageSections/content/ContentMainPage.js
+++ src/app/pageSections/content/ContentMainPage.js
@@ -1,11 +1,16 @@
+"use client";
+
 import BrandIcon from "@/app/universal_Components/BrandImage";
+import CustomCursor from "@/app/universal_Components/CustomCursor";
 
 export default function ContentMainPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <BrandIcon fill={400} />
-      Work in progress...... <br />
-      Latest work: Recruitment Status & Menu Bar
-    </div>
+    <CustomCursor>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <BrandIcon fill={400} />
+        Work in progress...... <br />
+        Latest work: Recruitment Status & Menu Bar
+      </div>
+    </CustomCursor>
   );
 }
